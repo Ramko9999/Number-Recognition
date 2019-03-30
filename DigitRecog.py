62af3a687be1484021c9a4425a857a01aed547b7
Ramko9999 <41026856+Ramko9999@users.noreply.github.com>
2019-03-30 15:06:00 -0500

    Add files via upload

---
+++ DigitRecog.py
@@ -0,0 +1,102 @@
+import os
+from numpy.linalg import svd
+import numpy as np
+import cv2
+import matplotlib.pylab as plt
+from time import time
+import random
+from Processer import organizeImages
+
+
+
+#this function will return the normalized version of the training data
+def featureScale(train_mat, possibleRange, possibleMean):
+
+    train_mat = train_mat - possibleMean
+    train_mat = train_mat / possibleRange
+
+    return train_mat
+
+
+#this function will return a reduced dimension array of the original inputs
+def dimReduce(train_mat, number_of_dims):
+
+    if(number_of_dims > np.shape(train_mat)[1]):
+        print("Number of dimensions cannot be greater than than original dimensions")
+        return None
+
+    #finding covariance matrix
+    m = np.shape(train_mat)[0]
+    covariance = np.dot(train_mat.T, train_mat) * 1/m # n x n
+
+    #applying SVD on the covariance mat
+    U, S, V = svd(covariance) # n x n
+
+    #actually finding the new reduced inputs
+    reducedFeatures = U[:, 0:number_of_dims] # n x k
+    reducedInputs = np.dot(train_mat, reducedFeatures) # m x k
+
+    return reducedInputs
+
+
+
+#used for quickly testing certain things
+def test():
+    a = np.random.rand(10 , 6)
+    print(a)
+    os.chdir("/Users/64000340/Desktop/Data")
+    print(os.listdir())
+
+
+
+
+
+#As of 3/26/2019: I am using Open CV to process images
+
+def main():
+
+    organizeImages()
+    n = 30 * 30 #dimensions of the pngs
+    os.chdir("/Users/64000340/Desktop/Train") #directory for folder for processing images
+    X = np.ones((1,n)) #dummy creation of the inputs matrix
+    Y = np.zeros((1,10)) #dummy creation of answers matrix
+
+    #getting each file in the directory
+    counter = 0
+    while(counter < 1185):
+        r = random.randint(0,9) # randomly ordering the training data
+
+        os.chdir("/Users/64000340/Desktop/Train/" + str(r)) # accessing the training folder for digit
+        if(len(os.listdir()) < 2):
+            continue
+        #manipulating the Y matrix in the same order as X
+        answers = [0,0,0,0,0,0,0,0,0,0]  #the len of the list represents the total classes in classification
+        answers[r] = 1
+        #appending the answers to the Y array
+        answers = np.array(answers)
+        Y = np.vstack((Y, answers))
+        #getting & reshaping the first digit image read as np array
+        sequence = cv2.imread(os.listdir()[1],0)
+        sequence = np.reshape(sequence, (1,900))
+        #adding it to our X array
+        X = np.vstack((X, sequence))
+        os.remove(os.listdir()[1])
+
+        counter+= 1
+
+
+    X = X[1:, :] #getting rid of the ones
+    Y = Y[1: , :] # m x 10
+    X = featureScale(X, 255, 127.5) # m x n
+
+    #adding the bias inputs to the X array
+    biasInputs = np.ones((np.shape(X)[0],1))
+    X = np.hstack((biasInputs, X))
+
+    print(np.shape(X), "Y shape", np.shape(Y))
+    print("X", X[100, :], "Y", Y[100, :])
+    theta1 = np.random.rand()
+
+start = time()
+main()
+print(time() - start)
---
+++ Processer.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+from time import time
+
+
+
+#the main purpose of this program is to reorganize the training data and put it back in the test folder
+def organizeImages():
+
+    baseDirectory = "/Users/64000340/Desktop/Numbers/"
+    targetDirectory = "/Users/64000340/Desktop/Train/"
+
+
+    for i in range(0, 10):
+        os.chdir(baseDirectory+ str(i)) #test directory
+        print(i)
+        counter = 0
+
+        for f in os.listdir():
+            #reading the image & resizing
+            if(f == ".DS_Store"):
+                continue
+            img = cv2.imread(f, 0)
+            img = cv2.resize(img, (30,30))
+            #writing to our test directory
+            os.chdir(targetDirectory + str(i))
+            cv2.imwrite(str(i) + " " + str(counter) + ".png", img) #creating a new gray scale image
+            #heading back to base directory
+            os.chdir(baseDirectory + str(i))
+            counter += 1
+
+
+#sending training data from a folder to the train folder: specific to user
+def sendImage():
+     baseDirectory = "/Users/64000340/Desktop/Extra/"
+     targetDirectory = "/Users/64000340/Desktop/Numbers/"
+     counter = 0
+     for i in range(0,10):
+         os.chdir(baseDirectory + str(i))
+         for file in os.listdir():
+             img = cv2.imread(file, 1)
+             os.chdir(targetDirectory + str(i))
+             cv2.imwrite(str(i) + str(counter) + ".png", img)
+             os.chdir(baseDirectory + str(i))
+             counter+= 1
